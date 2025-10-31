55706e3e21b89268326643cd72256553de5c5327
ROHIT6350 <rohitjadhav6350@gmail.com>
2025-10-31 20:00:50 +0530

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -39,6 +39,7 @@ module "ec2" {
   instance_type          = var.ec2_instance_type
   subnet_id              = element(module.vpc.public_subnets, 0)
   key_name               = var.ssh_key_name
+  create_new_key         = true
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 }
 
